b1b2372802fda41571e84b21a46c286056c1e4c9
sleepyjirachi1 <tickethandler@outlook.com>
2023-07-19 16:18:18 +0100

    Solved Task 2 Part 0 🛠️

---
+++ FBI/task_2_0.s
@@ -0,0 +1,16 @@
+.text
+.align 2
+.thumb
+.thumb_func
+
+main:
+    push {r0-r1, lr}
+    mov r0, #0xd
+    mov r1, #0x14
+    mul r1, r1, r0
+    ldr r0, =(0x8247094)
+    add r0, r0, r1
+    ldrb r0, [r0]
+    pop {r0-r1, pc}
+
+.align 2
